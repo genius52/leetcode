a67ea938ea4e52a8c6f8c68881de4a2d2498b727
genius52 <genius52@qq.com>
2020-04-17 17:59:56 +0800

    add dfs solution for 133

--- src/diagram/133. Clone Graph.go
+++ src/diagram/133. Clone Graph.go
@@ -11,36 +11,53 @@ func CloneGraph(node *Node) *Node {
 	if node == nil{
 		return nil
 	}
-	var head *Node = nil
-	var record map[int]*Node = make(map[int]*Node)
+	var head *Node = new(Node)
+	head.Val = node.Val
+	var record map[*Node]*Node = make(map[*Node]*Node)
+	record[node] = head
 	var q list.List
 	q.PushBack(node)
 	for q.Len() > 0{
 		top := q.Front()
-		var copy_node *Node
-		if _,ok := record[top.Value.(*Node).Val];ok {
-			copy_node = record[top.Value.(*Node).Val]
-		}else{
-			copy_node = new(Node)
-			copy_node.Val = top.Value.(*Node).Val
-			q.Remove(top)
-			if head == nil{
-				head = copy_node
-				record[copy_node.Val] = copy_node
-			}
-		}
+		copy_node := record[top.Value.(*Node)]
 		q.Remove(top)
 		for _, n := range top.Value.(*Node).Neighbors{
-			if _,ok := record[n.Val];ok{
-				copy_node.Neighbors = append(copy_node.Neighbors,record[n.Val])
+			if _,ok := record[n];ok{
+				copy_node.Neighbors = append(copy_node.Neighbors,record[n])
 				continue
 			}
 			var sub_node *Node = new(Node)
 			sub_node.Val = n.Val
-			record[sub_node.Val] = sub_node
+			record[n] = sub_node
 			copy_node.Neighbors = append(copy_node.Neighbors,sub_node)
 			q.PushBack(n)
 		}
 	}
 	return head
+}
+
+//dfs
+func dfs_cloneGraph(node *Node,record map[*Node]*Node)*Node{
+	if node == nil{
+		return nil
+	}
+	if _,ok := record[node];ok{
+		return record[node]
+	}
+	var copy *Node = new(Node)
+	copy.Val = node.Val
+	record[node] = copy
+	for _,n := range node.Neighbors{
+		copy.Neighbors = append(copy.Neighbors,dfs_cloneGraph(n,record))
+	}
+
+	return copy
+}
+
+func cloneGraph(node *Node) *Node {
+	if node == nil{
+		return nil
+	}
+	var record map[*Node]*Node = make(map[*Node]*Node)
+	return dfs_cloneGraph(node,record)
 }
