6622461fd22e49500925c4eda8dda5e6d1cbeee9
genius52 <genius52@qq.com>
2020-05-08 15:36:04 +0800

    331

--- src/golang/tree/331. Verify Preorder Serialization of a Binary Tree.go
+++ src/golang/tree/331. Verify Preorder Serialization of a Binary Tree.go
@@ -1,5 +1,29 @@
 package tree
 
-func isValidSerialization(preorder string) bool {
+import "strings"
 
+//2 = leaf
+//1 = half leaf
+//0 = not leaf
+//"9,3,4,#,#,1,#,#,2,#,6,#,#",
+func IsValidSerialization(preorder string) bool {
+	var record []string = strings.Split(preorder,",")
+	var stack []string
+	l := len(record)
+	for i := 0;i < l;i++{
+		len_s := len(stack)
+		if record[i] == "#"{
+			for len_s > 0 && stack[len_s - 1] == "#" {
+				stack = stack[:len_s - 1]
+				len_s = len(stack)
+				if len_s == 0{
+					return false
+				}
+				stack = stack[:len_s - 1]
+				len_s = len(stack)
+			}
+		}
+		stack = append(stack,record[i])
+	}
+	return len(stack) == 1 && stack[0] == "#"
 }
