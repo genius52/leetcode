835af3c8efe33a461d1fed94d5105287210130fa
genius52 <genius52@qq.com>
2020-06-14 09:31:01 +0800

    433 cpp file

--- src/cpp/graph/433. Minimum Genetic Mutation.hpp
+++ src/cpp/graph/433. Minimum Genetic Mutation.hpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <queue>
 using namespace std;
 
 class Solution_433 {
@@ -64,5 +65,31 @@ public:
         if(dfs_minMutation(start,end,res,graph,visited))
             return res;
         return -1;
+    }
+
+    int minMutation2(string start, string end, vector<string>& bank) {
+        int l = bank.size();
+        std::unordered_map<std::string,std::unordered_set<std::string>> graph;
+        for(int i = 0;i < l;i++){
+            for(int j = 0;j < l;j++){
+                if(i == j)
+                    continue;
+                if(is_mutation(bank[i],bank[j]))
+                    graph[bank[i]].insert(bank[j]);
+            }
+        }
+        for(int i = 0;i < l;i++){
+            if(is_mutation(bank[i],start))
+                graph[start].insert(bank[i]);
+        }
+        int res = 0;
+        std::unordered_set<std::string> visited;
+        std::queue<std::string> q;
+        q.push(start);
+        int cnt = 0;
+        while(q.empty()){
+            q.top();
+        }
+        return cnt;
     }
 };
