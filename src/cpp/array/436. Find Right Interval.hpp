1a17722635465ebded9261ad94183d98f3b511c1
genius52 <genius52@qq.com>
2020-06-20 10:15:22 +0800

    436 use map

---
+++ src/cpp/array/436. Find Right Interval.hpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+//Input: [ [3,4], [2,3], [1,2] ]
+//
+//Output: [-1, 0, 1]
+class Solution_436 {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        int len = intervals.size();
+        std::vector<int> res;
+        res.resize(len);
+        std::map<int,std::vector<int>> record;
+        for(int i = 0;i < len;i++){
+            record[intervals[i][0]].push_back(i);
+        }
+        for(int i = 0;i < len;i++){
+            auto it = record.lower_bound(intervals[i][1]);
+            if(it != record.end())
+                res[i] = (*it).second[0];
+            else
+                res[i] = -1;
+        }
+        return res;
+    }
+};
