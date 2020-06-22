84d86f4481e95d6c2e9cf4e1fefa88c36a0e8ee0
genius52 <genius52@qq.com>
2020-06-22 15:15:38 +0800

    450 by recursive

---
+++ src/cpp/tree/450. Delete Node in a BST.hpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include "../tree.h"
+
+class Solution_450 {
+    int find_max_inleft(TreeNode* node){
+        while(node->right != nullptr){
+            node = node->right;
+        }
+        return node->val;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(nullptr == root)
+            return root;
+        if(root->val < key){
+            root->right = deleteNode(root->right,key);
+        }else if(root->val > key){
+            root->left = deleteNode(root->left,key);
+        }
+        else if(root->val == key){
+            if(root->left == nullptr)
+                return root->right;
+            if(root->right == nullptr)
+                return root->left;
+            auto max_inleft = find_max_inleft(root->left);
+            root->val = max_inleft;
+            root->left = deleteNode(root->left,max_inleft);
+        }
+        return root;
+    }
+};
