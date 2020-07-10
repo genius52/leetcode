88623db3f263faae5eaec54f6c113facc7af8081
genius52 <genius52@qq.com>
2020-07-10 17:51:27 +0800

    532 using map

--- src/cpp/array/532. K-diff Pairs in an Array.hpp
+++ src/cpp/array/532. K-diff Pairs in an Array.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 
@@ -8,23 +9,49 @@ using namespace std;
 //Although we have two 1s in the input, we should only return the number of unique pairs.
 class Solution_532 {
 public:
+//    int findPairs(vector<int>& nums, int k) {
+//        std::sort(nums.begin(),nums.end());
+//        int len = nums.size();
+//        int res = 0;
+//        int begin = 0;
+//        while(begin < len - 1){
+//            while(begin > 0 && nums[begin] == nums[begin - 1])
+//                begin++;
+//            for(int j = begin + 1;j < len;j++){
+//                if(j > begin + 1 && nums[j] == nums[j - 1])
+//                    continue;
+//                if(nums[j] - nums[begin] == k)
+//                    res++;
+//                else if(nums[j] - nums[begin] > k)
+//                    break;
+//            }
+//            begin++;
+//        }
+//        return res;
+//    }
+
     int findPairs(vector<int>& nums, int k) {
-        std::sort(nums.begin(),nums.end());
+        if(k < 0){
+            return 0;
+        }
+        std::unordered_map<int,int> graph;
         int len = nums.size();
+        if(len <= 1)
+            return 0;
         int res = 0;
-        int begin = 0;
-        while(begin < len - 1){
-            while(begin > 0 && nums[begin] == nums[begin - 1])
-                begin++;
-            for(int j = begin + 1;j < len;j++){
-                if(j > begin + 1 && nums[j] == nums[j - 1])
-                    continue;
-                if(nums[j] - nums[begin] == k)
+        for(int i = 0;i < len;i++){
+            graph[nums[i]]++;
+        }
+        for(auto it = graph.begin();it != graph.end();it++){
+            if(k == 0){
+                if((*it).second > 1 ){
                     res++;
-                else if(nums[j] - nums[begin] > k)
-                    break;
+                }
+            }else{
+                if(graph.find((*it).first + k) != graph.end()){
+                    res++;
+                }
             }
-            begin++;
         }
         return res;
     }
