223f9c0f76dcc3fbf0e13521718e996cc828bc58
genius52 <genius52@qq.com>
2020-08-09 13:47:52 +0800

    274 275 brute force && binary search

--- src/golang/array/274. H-Index.go
+++ src/golang/array/274. H-Index.go
@@ -1,1 +1,18 @@
 package array
+
+import "sort"
+
+//Input: citations = [3,0,6,1,5]
+//Output: 3
+func HIndex(citations []int) int {
+	sort.Ints(citations)
+	var l int  = len(citations)
+	i := 0
+	for i < l{
+		if(citations[i] >= (l - i)){
+			break;
+		}
+		i++
+	}
+	return l - i
+}
--- src/golang/array/275. H-Index II.go
+++ src/golang/array/275. H-Index II.go
@@ -1,1 +1,24 @@
 package array
+
+//Input: citations = [0,1,3,5,6]
+//Output: 3
+func HIndex2(citations []int) int {
+	var l int = len(citations)
+	begin := 0
+	end := l - 1
+	for begin <= end{
+		var mid int = begin + (end - begin)/2
+		if(citations[mid] >= (l - mid)){
+			if(mid - 1 >= 0 && citations[mid - 1] < (l - mid + 1)){
+				return l - mid
+			}
+			end = mid - 1
+		}else{
+			if((mid + 1) < l && citations[mid + 1] >= (l - mid - 1)){
+				return l - mid - 1
+			}
+			begin = mid + 1
+		}
+	}
+	return l - begin
+}
