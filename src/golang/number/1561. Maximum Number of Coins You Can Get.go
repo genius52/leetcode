ca78b3708dd3dd29ca7b1140842db0d3b5c3d702
genius52 <genius52@qq.com>
2020-08-26 22:20:25 +0800

    1561 math problem

--- src/golang/number/1561. Maximum Number of Coins You Can Get.go
+++ src/golang/number/1561. Maximum Number of Coins You Can Get.go
@@ -1,1 +1,17 @@
 package number
+
+import "sort"
+
+func MaxCoins(piles []int) int {
+	var l int = len(piles)
+	var res int = 0
+	sort.Ints(piles)
+	begin := 0
+	end := l - 2
+	for begin <= end{
+		res += piles[end]
+		end -= 2
+		begin++
+	}
+	return res
+}
