7b1c7269d420f900b1488354e9f51490d7d1c7f4
genius52 <genius52@qq.com>
2020-09-21 15:39:50 +0800

    695 dfs

---
+++ src/golang/diagram/695. Max Area of Island.go
@@ -0,0 +1,34 @@
+package diagram
+
+func dfs_maxAreaOfIsland(grid [][]int,rows int,columns int,r int,c int)int{
+	if r < 0 || c < 0 || r >= rows || c >= columns{
+		return 0
+	}
+	if grid[r][c] == 0 || grid[r][c] == 2{
+		return 0
+	}
+	if grid[r][c] == 1{
+		grid[r][c] = 2
+		return 1 + dfs_maxAreaOfIsland(grid,rows,columns,r - 1,c) + dfs_maxAreaOfIsland(grid,rows,columns,r + 1,c) +
+			dfs_maxAreaOfIsland(grid,rows,columns,r,c - 1) + dfs_maxAreaOfIsland(grid,rows,columns,r,c + 1)
+	}
+	return 0
+}
+
+func maxAreaOfIsland(grid [][]int) int {
+	var rows int = len(grid)
+	var columns int = len(grid[0])
+	var res int = 0
+	for r := 0;r < rows;r++{
+		for c := 0;c < columns;c++{
+			if grid[r][c] != 1{
+				continue
+			}
+			areas := dfs_maxAreaOfIsland(grid,rows,columns,r,c)
+			if areas > res{
+				res = areas
+			}
+		}
+	}
+	return res
+}
