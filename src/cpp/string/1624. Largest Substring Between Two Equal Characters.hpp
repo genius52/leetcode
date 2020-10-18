1dcc1ff29492ead5f08b2b92cdf09b9e5ad2c47a
taochengcheng <taochengcheng@qiyi.com>
2020-10-18 14:52:54 +0800

    1624 by hashmap

---
+++ src/cpp/string/1624. Largest Substring Between Two Equal Characters.hpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution_1624 {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        std::unordered_map<char, int> begin_record;
+        std::unordered_map<char, int> end_record;
+        int len = s.size();
+        int res = -1;
+        for (int i = 0; i < len; i++) {
+            if (begin_record.find(s[i]) == begin_record.end()) {
+                begin_record[s[i]] = i;
+            }
+            else {
+                end_record[s[i]] = i;
+                int maxlen = i - begin_record[s[i]] - 1;
+                if (maxlen > 0) {
+                    if (maxlen > res) {
+                        res = maxlen;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
