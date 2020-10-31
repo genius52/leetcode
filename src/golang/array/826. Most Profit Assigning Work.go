d8b959bc1736c01e49bb82ec8af7d1b377f43e06
genius52 <genius52@qq.com>
2020-10-31 18:39:39 +0800

    826 by dp

--- src/golang/array/826. Most Profit Assigning Work.go
+++ src/golang/array/826. Most Profit Assigning Work.go
@@ -1,1 +1,43 @@
 package array
+
+func MaxProfitAssignment(difficulty []int, profit []int, worker []int) int {
+	var record map[int]int = make(map[int]int)
+	var l int = len(difficulty)
+	var low int = 100000
+	var high int = 1
+	for i := 0;i < l;i++{
+		if pre_profit,ok := record[difficulty[i]];ok{
+			if pre_profit < profit[i]{
+				record[difficulty[i]] = profit[i]
+			}
+		}else{
+			record[difficulty[i]] = profit[i]
+		}
+
+		if difficulty[i] < low{
+			low = difficulty[i]
+		}
+		if difficulty[i] > high{
+			high = difficulty[i]
+		}
+	}
+	var dp []int = make([]int,100001)
+	var max_profit int = 0
+	for i := low;i <= 100000;i++{
+		if profit,ok := record[i];ok{
+			if profit > max_profit{
+				max_profit = profit
+			}
+			dp[i] = max_profit
+		}else{
+			dp[i] = max_profit
+		}
+	}
+
+	var res int = 0
+	var l2 int = len(worker)
+	for i := 0;i < l2;i++{
+		res += dp[worker[i]]
+	}
+	return res
+}
--- src/golang/main/main.go
+++ src/golang/main/main.go
@@ -12,6 +12,16 @@ import (
 
 func main(){
 	{
+		//var difficulty []int = []int{2,4,6,8,10}
+		//var profit []int = []int{10,20,30,40,50}
+		//var worker []int = []int{4,5,6,7}
+		var difficulty []int = []int{66,1,28,73,53,35,45,60,100,44,59,94,27,88,7,18,83,18,72,63}
+		var profit []int = 	   []int{66,20,84,81,56,40,37,82,53,45,43,96,67,27,12,54,98,19,47,77}
+		var worker []int = []int{61,33,68,38,63,45,1,10,53,23,66,70,14,51,94,18,28,78,100,16}
+		res := array.MaxProfitAssignment(difficulty,profit,worker)
+		fmt.Println(res)
+	}
+	{
 		//var intervals [][]int = [][]int{{1,2},{3,5},{6,7},{8,10},{12,16}}
 		//var newInterval []int = []int{16,18}
 		var intervals [][]int = [][]int{{1,5}}
