e6890499e7812423b72575e448ddce8a8da9b82c
genius52 <genius52@qq.com>
2020-12-05 18:40:23 +0800

    1020 by dfs mark

---
+++ src/golang/diagram/1020. Number of Enclaves.go
@@ -0,0 +1,45 @@
+package diagram
+
+func dfs_numEnclaves(A [][]int,r int,c int,rows int,columns int){
+	if r < 0 || r >= rows || c < 0 || c >= columns{
+		return
+	}
+	if A[r][c] == 0 || A[r][c] == 2{
+		return
+	}
+	A[r][c] = 2
+	dfs_numEnclaves(A,r - 1,c,rows,columns)
+	dfs_numEnclaves(A,r + 1,c,rows,columns)
+	dfs_numEnclaves(A,r,c - 1,rows,columns)
+	dfs_numEnclaves(A,r,c + 1,rows,columns)
+}
+
+func numEnclaves(A [][]int) int {
+	var rows int = len(A)
+	var columns int = len(A[0])
+	for i := 0;i < rows;i++{
+		if A[i][0] != 0  && A[i][0] != 2{
+			dfs_numEnclaves(A,i,0,rows,columns)
+		}
+		if A[i][columns - 1] != 0 && A[i][columns - 1] != 2{
+			dfs_numEnclaves(A,i,columns - 1,rows,columns)
+		}
+	}
+	for j := 0;j < columns;j++{
+		if A[0][j] != 0 && A[0][j] != 2{
+			dfs_numEnclaves(A,0,j,rows,columns)
+		}
+		if A[rows - 1][j] != 0 && A[rows - 1][j] != 2{
+			dfs_numEnclaves(A,rows - 1,j,rows,columns)
+		}
+	}
+	var res int = 0
+	for i := 1;i < rows - 1;i++{
+		for j := 1;j < columns;j++{
+			if A[i][j] == 1{
+				res++
+			}
+		}
+	}
+	return res
+}
