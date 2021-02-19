46ab741ac0ef7460ac31566875d63d9f3113de52
genius52 <genius52@qq.com>
2021-02-19 10:38:37 +0800

    72 add dp from bottom to top solution

--- src/golang/string_issue/72. Edit Distance.go
+++ src/golang/string_issue/72. Edit Distance.go
@@ -51,3 +51,34 @@ func minDistance(word1 string, word2 string) int {
 	var record map[string]int = make(map[string]int)
 	return dp_minDistance(word1,0,word2,0,record)
 }
+
+func minDistance2(word1 string, word2 string) int {
+	var len1 int = len(word1)
+	var len2 int = len(word2)
+	if len1 == 0{
+		return len2
+	}
+	if len2 == 0{
+		return len1
+	}
+	var dp [][]int = make([][]int,len1 + 1)//dp[i][j] = min steps of change word1[0:i] to word2[0:j]
+	for i := 0;i <= len1;i++{
+		dp[i] = make([]int,len2 + 1)
+	}
+	for i := 1;i <= len1;i++{
+		dp[i][0] = i
+	}
+	for j := 1;j <= len2;j++{
+		dp[0][j] = j
+	}
+	for i := 1;i <= len1;i++{
+		for j := 1;j <= len2;j++{
+			if word1[i - 1] == word2[j - 1]{
+				dp[i][j] = dp[i - 1][j - 1]
+			}else{
+				dp[i][j] = min_int_number(dp[i][j - 1],dp[i - 1][j],dp[i - 1][j - 1]) + 1
+			}
+		}
+	}
+	return dp[len1][len2]
+}
