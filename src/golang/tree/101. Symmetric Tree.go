5d8e8f26108482839b610a9263a2a5e55fb6eba8
genius52 <genius52@qq.com>
2021-02-22 08:18:43 +0800

    101 by recursive way

---
+++ src/golang/tree/101. Symmetric Tree.go
@@ -0,0 +1,18 @@
+package tree
+
+func dfs_isSymmetric(left *TreeNode,right *TreeNode)bool{
+	if left == nil && right == nil{
+		return true
+	}
+	if (left == nil && right != nil) || (left != nil && right == nil){
+		return false
+	}
+	if left.Val != right.Val{
+		return false
+	}
+	return dfs_isSymmetric(left.Left,right.Right) || dfs_isSymmetric(left.Right,right.Left)
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return dfs_isSymmetric(root.Left,root.Right)
+}
