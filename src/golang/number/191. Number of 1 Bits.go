b4713a1e115f532af4eeaff341ab76dc2ac97dc1
genius52 <genius52@qq.com>
2021-03-01 15:10:16 +0800

    191 bit checking

---
+++ src/golang/number/191. Number of 1 Bits.go
@@ -0,0 +1,18 @@
+package number
+
+//Input: n = 00000000000000000000000000001011
+//Output: 3
+//Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+func hammingWeight(num uint32) int {
+	var res int = 0
+	for i := 0;i < 32;i++{
+		if (num & 1) == 1{
+			res++
+		}
+		num = num >> 1
+		if num == 0{
+			break
+		}
+	}
+	return res
+}
