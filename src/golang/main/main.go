1dfafc7d98812cd471bd37d528b0af551afdb0cd
genius52 <genius52@qq.com>
2021-03-09 10:26:03 +0800

    233 brute force by recursive way.There is an one-line solution

--- src/golang/main/main.go
+++ src/golang/main/main.go
@@ -13,6 +13,11 @@ import (
 )
 func main(){
 	{
+		var n int = 38
+		res := number.AddDigits(n)
+		fmt.Println(res)
+	}
+	{
 		var matrix [][]int = [][]int{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}}
 		var target int = 5
 		res := array.SearchMatrix240(matrix,target)
--- src/golang/number/233. Number of Digit One.go
+++ src/golang/number/233. Number of Digit One.go
@@ -1,1 +1,23 @@
 package number
+
+//Input: n = 13
+//Output: 6
+func cacl_one(n int)int{
+	var ones int = 0
+	for n != 0{
+		mod := n % 10
+		if mod == 1{
+			ones++
+		}
+		n = n / 10
+	}
+	return ones
+}
+
+func countDigitOne(n int) int {
+	var total int = 0
+	for i := 1;i <= n;i++{
+		total += cacl_one(i)
+	}
+	return total
+}
