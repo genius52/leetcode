0d3df0c5bbf76b26df3fd4dc15c2d3441d564db5
genius52 <genius52@qq.com>
2021-03-20 20:05:56 +0800

    389 char count issue

---
+++ src/golang/string_issue/389. Find the Difference.go
@@ -0,0 +1,18 @@
+package string_issue
+
+func findTheDifference(s string, t string) byte {
+	var cnt [26]int
+	for _,c := range t{
+		cnt[c - 'a']++
+	}
+	for _,c := range s{
+		cnt[c - 'a']--
+	}
+	var res byte
+	for i := 0;i < 26;i++{
+		if cnt[i] == 1{
+			res = byte(i) + byte('a')
+		}
+	}
+	return res
+}
