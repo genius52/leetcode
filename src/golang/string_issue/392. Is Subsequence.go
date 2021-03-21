87911902eedcb3fdd3b3c44dd01b2b612c0707ad
genius52 <genius52@qq.com>
2021-03-21 15:37:29 +0800

    392 optimize time consume

---
+++ src/golang/string_issue/392. Is Subsequence.go
@@ -0,0 +1,24 @@
+package string_issue
+
+//func dp_isSubsequence(s string,l1 int,pos1 int,t string,l2 int,pos2 int)bool{
+//	if pos2 >= l2 && l1
+//}
+
+func isSubsequence(s string, t string) bool {
+	var l1 int = len(s)
+	var l2 int = len(t)
+	var pos1 int = 0
+	var pos2 int = 0
+	for pos1 < l1 && pos2 < l2{
+		if s[pos1] == t[pos2]{
+			pos1++
+			pos2++
+		}else{
+			pos2++
+		}
+	}
+	if pos1 < l1{
+		return false
+	}
+	return true
+}
