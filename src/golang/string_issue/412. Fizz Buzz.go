c3f44aab2620dd08f4155398ff657c35af4a4387
genius52 <genius52@qq.com>
2021-03-25 13:31:09 +0800

    412 simple math issue

---
+++ src/golang/string_issue/412. Fizz Buzz.go
@@ -0,0 +1,47 @@
+package string_issue
+
+import "strconv"
+
+//Write a program that outputs the string representation of numbers from 1 to n.
+//
+//But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+//
+//Example:
+//
+//n = 15,
+//
+//Return:
+//[
+//    "1",
+//    "2",
+//    "Fizz",
+//    "4",
+//    "Buzz",
+//    "Fizz",
+//    "7",
+//    "8",
+//    "Fizz",
+//    "Buzz",
+//    "11",
+//    "Fizz",
+//    "13",
+//    "14",
+//    "FizzBuzz"
+//]
+func fizzBuzz(n int) []string {
+	var res []string = make([]string,n)
+	for i := 1;i <= n;i++{
+		mod3 := i % 3
+		mod5 := i % 5
+		if mod3 == 0 && mod5 == 0{
+			res[i - 1] = "FizzBuzz"
+		}else if mod3 == 0{
+			res[i - 1] = "Fizz"
+		}else if mod5 == 0{
+			res[i - 1] = "Buzz"
+		}else{
+			res[i - 1] = strconv.Itoa(i)
+		}
+	}
+	return res
+}
