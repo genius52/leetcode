9744979ca68c6ad41a7c326243933afff2e7a2b2
genius52 <genius52@qq.com>
2021-03-30 09:25:04 +0800

    429 bfs

---
+++ src/golang/tree/429. N-ary Tree Level Order Traversal.go
@@ -0,0 +1,30 @@
+package tree
+
+import "container/list"
+type Node2 struct {
+	Val int
+	Children []*Node
+}
+
+func levelOrder429(root *Node2) [][]int {
+	var res [][]int
+	if root == nil{
+		return res
+	}
+	var q list.List
+	q.PushBack(root)
+	for q.Len() > 0{
+		var l int = q.Len()
+		var cur []int
+		for i := 0;i < l;i++{
+			var node *Node2 = q.Front().Value.(*Node2)
+			q.Remove(q.Front())
+			cur = append(cur,node.Val)
+			for _,child := range node.Children{
+				q.PushBack(child)
+			}
+		}
+		res = append(res,cur)
+	}
+	return res
+}
