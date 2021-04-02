46822b9fa6953416cc6d6ded39578427a9606a6f
genius52 <genius52@qq.com>
2021-04-02 17:07:34 +0800

    445 by stack

---
+++ src/golang/list_queue/445. Add Two Numbers II.go
@@ -0,0 +1,78 @@
+package list_queue
+
+import "container/list"
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var q1 list.List
+	var q2 list.List
+	for l1 != nil{
+		q1.PushFront(l1)
+		l1 = l1.Next
+	}
+	for l2 != nil{
+		q2.PushFront(l2)
+		l2 = l2.Next
+	}
+	var pre *ListNode = nil
+	var upgrade bool = false
+	for q1.Len() > 0 && q2.Len() > 0{
+		var n1 *ListNode = q1.Front().Value.(*ListNode)
+		var n2 *ListNode = q2.Front().Value.(*ListNode)
+		q1.Remove(q1.Front())
+		q2.Remove(q2.Front())
+		var n ListNode
+		n.Val = n1.Val + n2.Val
+		if upgrade{
+			n.Val++
+		}
+		if n.Val >= 10{
+			n.Val -= 10
+			upgrade = true
+		}else{
+			upgrade = false
+		}
+		n.Next = pre
+		pre = &n
+	}
+	for q1.Len() > 0{
+		var n1 *ListNode = q1.Front().Value.(*ListNode)
+		q1.Remove(q1.Front())
+		var n ListNode
+		n.Val = n1.Val
+		if upgrade{
+			n.Val++
+		}
+		if n.Val >= 10{
+			n.Val -= 10
+			upgrade = true
+		}else{
+			upgrade = false
+		}
+		n.Next = pre
+		pre = &n
+	}
+	for q2.Len() > 0{
+		var n2 *ListNode = q2.Front().Value.(*ListNode)
+		q2.Remove(q2.Front())
+		var n ListNode
+		n.Val = n2.Val
+		if upgrade{
+			n.Val++
+		}
+		if n.Val >= 10{
+			n.Val -= 10
+			upgrade = true
+		}else{
+			upgrade = false
+		}
+		n.Next = pre
+		pre = &n
+	}
+	if upgrade{
+		var n ListNode
+		n.Val = 1
+		n.Next = pre
+		pre = &n
+	}
+	return pre
+}
