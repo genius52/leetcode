1652279222abd5b0806400e672895697811e0394
genius52 <genius52@qq.com>
2021-04-06 08:11:41 +0800

    452 sort by end postion

--- src/cpp/graph/452. Minimum Number of Arrows to Burst Balloons.hpp
+++ src/cpp/graph/452. Minimum Number of Arrows to Burst Balloons.hpp
@@ -29,4 +29,27 @@ public:
         }
         return cnt;
     }
+
+    int findMinArrowShots2(vector<vector<int>>& points) {
+        int len = points.size();
+        if(len <= 1)
+            return len;
+        auto my_sort = [](const std::vector<int>& v1,const std::vector<int>& v2){
+            if(v1[1] == v2[1]){
+                return v1[0] < v2[0];
+            }
+            return v1[1] < v2[1];
+        };
+        std::sort(points.begin(),points.end(),my_sort);
+        int pre_shot_pos = points[0][1];
+        int dup_cnt = 0;
+        for(int i = 1;i < len;i++){
+            if(points[i][0] <= pre_shot_pos){
+                dup_cnt++;
+             }else{
+                pre_shot_pos = points[i][1];
+            }
+        }
+        return len - dup_cnt;
+    }
 };
