169aff0c55e67399b6b24cd115b4e2d00e1b62cb
genius52 <genius52@qq.com>
2021-04-10 09:44:54 +0800

    502 optimize by priority queue

--- src/cpp/graph/502. IPO.hpp
+++ src/cpp/graph/502. IPO.hpp
@@ -11,36 +11,27 @@ using namespace std;
 class Solution_502 {
 public:
     int findMaximizedCapital(int k, int W, vector<int>& Profits, vector<int>& Capital) {
-        std::map<int,std::multiset<int,std::greater<int>>> m;
+        std::map<int,std::vector<int>> m;
         int len = Profits.size();
         for(int i = 0;i < len;i++){
-            m[Capital[i]].insert(Profits[i]);
+            m[Capital[i]].push_back(Profits[i]);
         }
+        std::priority_queue<int> q;
+        std::map<int,std::vector<int>>::iterator it = m.begin();
         int cur_capital = W;
         for(int i = 0;i < k;i++){
-            int cur_max_pro = 0;
-            int key = -1;
-            std::multiset<int>::iterator itval;
-            for(auto it = m.begin();it != m.end();it++) {
+            for(;it != m.end();it++) {
                 if(cur_capital < it->first)
                     break;
-                int pro = *it->second.begin();
-                if(pro > cur_max_pro) {
-                    cur_max_pro = pro;
-                    key = it->first;
-                    itval = it->second.begin();
+                vector<int> tmp = it->second;
+                for(auto it2 = tmp.begin();it2 != tmp.end();it2++) {
+                    q.push(*it2);
                 }
             }
-            if(key != -1){
-                if(m[key].size() == 1){
-                    m.erase(key);
-                }else{
-                    m[key].erase(itval);
-                }
-            }else{
+            if(q.empty())
                 break;
-            }
-            cur_capital += cur_max_pro;
+            cur_capital += q.top();
+            q.pop();
         }
         return cur_capital;
     }
