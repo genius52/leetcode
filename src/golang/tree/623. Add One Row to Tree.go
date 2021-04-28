5531ccdcf6001de70c719be61d76da3b057162b1
genius52 <genius52@qq.com>
2021-04-28 09:13:58 +0800

    623 add no-recursive solution

--- src/golang/tree/623. Add One Row to Tree.go
+++ src/golang/tree/623. Add One Row to Tree.go
@@ -1,5 +1,7 @@
 package tree
 
+import "container/list"
+
 //Input:
 //A binary tree as following:
 //       4
@@ -55,5 +57,51 @@ func AddOneRow(root *TreeNode, v int, d int) *TreeNode {
 	}
 	root.Left = visit_addOneRow(root,v,2,d,true)
 	root.Right = visit_addOneRow(root,v,2,d,false)
+	return root
+}
+
+func addOneRow2(root *TreeNode, val int, depth int) *TreeNode {
+	if root == nil{
+		return nil
+	}
+	if depth == 1{
+		var new_root TreeNode
+		new_root.Val = val
+		new_root.Left = root
+		return &new_root
+	}
+	var q list.List
+	q.PushBack(root)
+	for depth > 1{
+		var l int = q.Len()
+		for i := 0;i < l;i++{
+			var node *TreeNode = q.Front().Value.(*TreeNode)
+			q.Remove(q.Front())
+			if depth == 2{
+				pre_left := node.Left
+				pre_right := node.Right
+				var new_node1 TreeNode
+				new_node1.Val = val
+				node.Left = &new_node1
+				if pre_left != nil{
+					new_node1.Left = pre_left
+				}
+				var new_node2 TreeNode
+				new_node2.Val = val
+				node.Right = &new_node2
+				if pre_right != nil{
+					new_node2.Right = pre_right
+				}
+			}else{
+				if node.Left != nil{
+					q.PushBack(node.Left)
+				}
+				if node.Right != nil{
+					q.PushBack(node.Right)
+				}
+			}
+		}
+		depth--
+	}
 	return root
 }
