24de2bee247c4c1ad2d39eef7793513ec0ecc562
genius52 <genius52@qq.com>
2021-05-07 09:43:07 +0800

    669 add golang solution

---
+++ src/golang/tree/669. Trim a Binary Search Tree.go
@@ -0,0 +1,20 @@
+package tree
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil{
+		return nil
+	}
+	left := trimBST(root.Left,low,high)
+	right := trimBST(root.Right,low,high)
+	if root.Val < low || root.Val > high{//
+		if left != nil{
+			return left
+		}else{
+			return right
+		}
+	}else{
+		root.Left = left
+		root.Right = right
+		return root
+	}
+}
