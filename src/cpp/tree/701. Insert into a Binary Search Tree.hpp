f899e5912a5c9a2a9691142d19a3bd9198588c8d
genius52 <genius52@qq.com>
2021-05-14 08:16:50 +0800

    701 optimize code

--- src/cpp/tree/701. Insert into a Binary Search Tree.hpp
+++ src/cpp/tree/701. Insert into a Binary Search Tree.hpp
@@ -2,33 +2,46 @@
 
 class Solution_701 {
 public:
-    void insertIntoBST(TreeNode* node,int val){
-        if(nullptr == node)
-            return;
-
+    TreeNode* insertIntoBST(TreeNode* node,int val){
+        if(node == nullptr){
+            return new TreeNode(val);
+        }
         if(val < node->val){
-            if(nullptr == node->left){
-                TreeNode* new_node = new TreeNode(val);
-                node->left = new_node;
-                return;
-            }
-            else
-                preorder_visit(node->left,val);
-        }
-        else{
-            if(nullptr == node->right){
-                TreeNode* new_node = new TreeNode(val);
-                node->right = new_node;
-                return;
-            }
-            preorder_visit(node->right,val);
+            node->left = insertIntoBST(node->left,val);
+            return node;
+        }else{
+            node->right = insertIntoBST(node->right,val);
+            return node;
         }
     }
 
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(nullptr == root)
-            return root;
-        preorder_visit(root,val);
-        return root;
-    }
+//    void insertIntoBST(TreeNode* node,int val){
+//        if(nullptr == node)
+//            return;
+//
+//        if(val < node->val){
+//            if(nullptr == node->left){
+//                TreeNode* new_node = new TreeNode(val);
+//                node->left = new_node;
+//                return;
+//            }
+//            else
+//                preorder_visit(node->left,val);
+//        }
+//        else{
+//            if(nullptr == node->right){
+//                TreeNode* new_node = new TreeNode(val);
+//                node->right = new_node;
+//                return;
+//            }
+//            preorder_visit(node->right,val);
+//        }
+//    }
+
+//    TreeNode* insertIntoBST(TreeNode* root, int val) {
+//        if(nullptr == root)
+//            return root;
+//        preorder_visit(root,val);
+//        return root;
+//    }
 };
