591ab1edd123c6353b4c111ee6018b9ab0349466
genius52 <genius52@qq.com>
2021-05-20 10:00:16 +0800

    718 LCS solution by TCC way

--- src/golang/array/718. Maximum Length of Repeated Subarray.go
+++ src/golang/array/718. Maximum Length of Repeated Subarray.go
@@ -8,6 +8,7 @@ import "math"
 //Output: 3
 //Explanation:
 //The repeated subarray with maximum length is [3, 2, 1].
+//standard LCS from bottom to top
 func FindLength(A []int, B []int) int {
 	var len_a int = len(A)
 	var len_b int = len(B)
@@ -21,6 +22,32 @@ func FindLength(A []int, B []int) int {
 			if A[i] == B[j]{
 				dp[i+1][j+1] = dp[i][j] + 1
 				res = int(math.Max(float64(dp[i+1][j+1]),float64(res)))
+			}
+		}
+	}
+	return res
+}
+
+//LCS
+func FindLengthex(A []int, B []int) int {
+	var len_a int = len(A)
+	var len_b int = len(B)
+	var dp [][]int = make([][]int,len_a)//dp[i][j] = longest length A[:i)    b[0:j)
+	for i := 0;i < len_a;i++{
+		dp[i] = make([]int,len_b)
+	}
+	var res int = 0
+	for i := 0;i < len_a;i++{
+		for j := 0;j < len_b;j++{
+			if A[i] == B[j]{
+				if i == 0 || j == 0{
+					dp[i][j] = 1
+				}else{
+					dp[i][j] = int(math.Max(float64(dp[i - 1][j - 1] + 1),float64(dp[i][j])))
+				}
+				if res < dp[i][j]{
+					res = dp[i][j]
+				}
 			}
 		}
 	}
