cf9c4ba999e226c7c0a20c978dc173a360d75201
genius52 <genius52@qq.com>
2021-05-21 11:17:33 +0800

    729 using map instead of set

--- src/cpp/list/729. My Calendar I.hpp
+++ src/cpp/list/729. My Calendar I.hpp
@@ -1,25 +1,25 @@
-#include <set>
+#include <map>
 using namespace std;
 
 class MyCalendar {
-    std::set<std::pair<int,int>> start_end;
+    std::map<int,int> start_end;
 public:
     MyCalendar() {
 
     }
 
     bool book(int start, int end) {
-        //在已有开始时间中查找小于等于当前的结束时间
-        for(auto it = start_end.begin();it != start_end.end();it++){
-            if(it->first >= end){
-                break;
-            }
-            if(max(it->first,start) < min(it->second,end)){
-                return false;
-            }
+        auto it = start_end.lower_bound(end);
+        if(it == start_end.begin()){
+            start_end.insert({start,end});
+            return true;
         }
-        start_end.insert(std::pair<int,int>{start,end});
-        return true;
+        it--;
+        if((*it).second <= start){
+            start_end.insert({start,end});
+            return true;
+        }
+        return false;
     }
 };
 
