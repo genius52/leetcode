ffd9b5121b31a41d3a95e9a129430c878d1e8a12
genius52 <genius52@qq.com>
2021-06-04 15:45:20 +0800

    799 optimize performance

--- src/golang/number/799. Champagne Tower.go
+++ src/golang/number/799. Champagne Tower.go
@@ -4,9 +4,9 @@ func ChampagneTower(poured int, query_row int, query_glass int) float64{
 	if poured <= 0 {
 		return 0.0
 	}
-	var dp [][]float64 = make([][]float64,101)
-	for i := 0;i < 101;i++{
-		dp[i] = make([]float64,101)
+	var dp [][]float64 = make([][]float64,query_row + 2)
+	for i := 0;i <= query_row + 1;i++{
+		dp[i] = make([]float64,i + 1)
 	}
 	dp[0][0] = float64(poured)
 	for r := 0;r <= query_row;r++{
@@ -20,19 +20,3 @@ func ChampagneTower(poured int, query_row int, query_glass int) float64{
 	}
 	return dp[query_row][query_glass]
 }
-
-//func champagneTower(poured int, query_row int, query_glass int) float64 {
-	//if poured <= 0 || query_row > poured / 2{
-	//	return 0.0
-	//}
-	//var total float64 = float64((2 + query_row)* (query_row + 1)/ 2)
-	//if int(total) <= poured{
-	//	return 1.0
-	//}
-	//var total2 float64 = float64((1 + query_row)* (query_row)/ 2)
-	//var rest float64 = float64(poured) - total2
-	//if query_glass == 0 || query_glass == query_row{
-	//	return 0.0
-	//}
-	//return rest/float64(query_row - 1)
-//}
