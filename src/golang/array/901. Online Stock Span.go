5f7c0964ae006d0d3c3bba61044d06d434828b7c
genius52 <genius52@qq.com>
2021-07-02 10:18:54 +0800

    901 implement by stack

--- src/golang/array/901. Online Stock Span.go
+++ src/golang/array/901. Online Stock Span.go
@@ -1,9 +1,14 @@
 package array
 
+import "container/list"
+
+type Price_duration struct {
+	price int
+	duration int
+}
+
 type StockSpanner struct {
-	prices []int
-	dp []int
-	length int
+	q list.List
 }
 
 func Constructor901() StockSpanner {
@@ -11,30 +16,51 @@ func Constructor901() StockSpanner {
 	return obj
 }
 
-
 func (this *StockSpanner) Next(price int) int {
-	if this.length > 0 && price >= this.prices[this.length - 1]{
-		//this.dp = append(this.dp,this.dp[this.length - 1] + 1)
-		visit := this.length - 1
-		for visit >=0 {
-			if price >= this.prices[visit]{
-				visit = visit - this.dp[visit]
-			}else{
-				break
-			}
+	var res int = 1
+	for this.q.Len() > 0{
+		var cur Price_duration = this.q.Back().Value.(Price_duration)
+		if cur.price <= price{
+			res += cur.duration
+			this.q.Remove(this.q.Back())
+		}else{
+			break
 		}
-		this.dp = append(this.dp,this.length - visit)
-	}else{
-		this.dp = append(this.dp,1)
 	}
-	this.prices = append(this.prices,price)
-	this.length++
-	return this.dp[this.length - 1]
+	var obj Price_duration
+	obj.price = price
+	obj.duration = res
+	this.q.PushBack(obj)
+	return res
 }
 
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * obj := Constructor();
- * param_1 := obj.Next(price);
- */
+//type StockSpanner struct {
+//	prices []int
+//	dp []int
+//	length int
+//}
+//
+//func Constructor901() StockSpanner {
+//	var obj StockSpanner
+//	return obj
+//}
+//
+//
+//func (this *StockSpanner) Next(price int) int {
+//	if this.length > 0 && price >= this.prices[this.length - 1]{
+//		visit := this.length - 1
+//		for visit >=0 {
+//			if price >= this.prices[visit]{
+//				visit = visit - this.dp[visit]
+//			}else{
+//				break
+//			}
+//		}
+//		this.dp = append(this.dp,this.length - visit)
+//	}else{
+//		this.dp = append(this.dp,1)
+//	}
+//	this.prices = append(this.prices,price)
+//	this.length++
+//	return this.dp[this.length - 1]
+//}
