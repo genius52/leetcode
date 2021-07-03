446e1d0de1ca94d52a86325aca986fe59cab003c
genius52 <genius52@qq.com>
2021-07-03 21:23:52 +0800

    add files to pass compile

--- src/golang/array/891. Sum of Subsequence Widths.go
+++ src/golang/array/891. Sum of Subsequence Widths.go
@@ -1,1 +1,23 @@
 package array
+
+import (
+	"math"
+	"sort"
+)
+
+//Input: nums = [2,1,3]
+//Output: 6
+//Explanation:
+//Subsequences are [1], [2], [3], [2,1], [2,3], [1,3], [2,1,3].
+//The corresponding widths are 0, 0, 0, 1, 1, 2, 2.
+//The sum of these widths is 6.
+func SumSubseqWidths(nums []int) int {
+	sort.Ints(nums)
+	var l int = len(nums)
+	var res int64 = 0
+	for i := 0;i < l;i++{
+		//nums[i] as the biggest number
+		res = (res + int64(nums[i]) * (int64(math.Pow(2,float64(i))) % 1000000007) - int64(nums[i]) * (int64(math.Pow(2,float64(l - i - 1))) % 1000000007)) % 1000000007
+	}
+	return int(res + 1000000007) % 1000000007
+}
--- src/golang/array/903. Valid Permutations for DI Sequence.go
+++ src/golang/array/903. Valid Permutations for DI Sequence.go
@@ -1,1 +1,97 @@
 package array
+
+//Input: s = "DID"
+//Output: 5
+//Explanation:
+//The 5 valid permutations of (0, 1, 2, 3) are:
+//(1, 0, 3, 2)
+//(2, 0, 3, 1)
+//(2, 1, 3, 0)
+//(3, 0, 2, 1)
+//(3, 1, 2, 0)
+
+// dp from top to bottom (TLE)
+func dfs_numPermsDISequence(s string,l int,cur_pos int,data []int,visited []bool)int{
+	if cur_pos >= l{
+		for i := 0;i <= l;i++{
+			if !visited[i]{
+				if s[cur_pos - 1] == 'D'{
+					if data[cur_pos - 1] < i{
+						return 0
+					}else{
+						return 1
+					}
+				}else{
+					if data[cur_pos - 1] < i{
+						return 1
+					}else{
+						return 0
+					}
+				}
+			}
+		}
+	}
+	var res int = 0
+	if s[cur_pos] == 'D'{
+		for i := l;i >= 1;i--{
+			if visited[i]{
+				continue
+			}
+			if cur_pos == 0{
+				visited[i] = true
+				data[cur_pos] = i
+				res += dfs_numPermsDISequence(s,l,cur_pos + 1,data,visited)
+				visited[i] = false
+			}else{
+				if s[cur_pos - 1] == 'D'{
+					if i > data[cur_pos - 1]{
+						continue
+					}
+				}else{
+					if i < data[cur_pos - 1]{
+						continue
+					}
+				}
+				visited[i] = true
+				data[cur_pos] = i
+				res += dfs_numPermsDISequence(s,l,cur_pos + 1,data,visited)
+				visited[i] = false
+			}
+		}
+	}else if s[cur_pos] == 'I'{
+		for i := 0;i < l;i++{
+			if visited[i]{
+				continue
+			}
+			if cur_pos == 0{
+				visited[i] = true
+				data[cur_pos] = i
+				res += dfs_numPermsDISequence(s,l,cur_pos + 1,data,visited)
+				visited[i] = false
+			}else{
+				if s[cur_pos - 1] == 'D'{
+					if i > data[cur_pos - 1]{
+						continue
+					}
+				}else{
+					if i < data[cur_pos - 1]{
+						continue
+					}
+				}
+				visited[i] = true
+				data[cur_pos] = i
+				res += dfs_numPermsDISequence(s,l,cur_pos + 1,data,visited)
+				visited[i] = false
+			}
+		}
+	}
+	return res
+}
+
+func NumPermsDISequence(s string) int {
+	var l int = len(s)
+	var data []int = make([]int,l + 1)
+	var visited []bool = make([]bool,l + 1)
+	var res int = dfs_numPermsDISequence(s,l,0,data,visited)
+	return res
+}
