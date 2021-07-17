c94ef6d66986642903e6ab8f3f3bf6f4a8df2272
genius52 <genius52@qq.com>
2021-07-17 20:28:50 +0800

    958 optimize tree visit by bfs way

--- src/golang/tree/958. Check Completeness of a Binary Tree.go
+++ src/golang/tree/958. Check Completeness of a Binary Tree.go
@@ -1,5 +1,7 @@
 package tree
 
+import "container/list"
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -8,30 +10,75 @@ package tree
  *     Right *TreeNode
  * }
  */
-func visit_isCompleteTree(node *TreeNode,parent int,cur int,record map[int][]int){
+//BFS solution
+func cnt_isCompleteTree(node *TreeNode) int{
 	if node == nil{
-		return
+		return 0
 	}
-	record[parent] = append(record[parent],cur)
-	visit_isCompleteTree(node.Left,cur,cur * 2,record)
-	visit_isCompleteTree(node.Right,cur,cur * 2 + 1,record)
+	return 1 + cnt_isCompleteTree(node.Left) + cnt_isCompleteTree(node.Right)
 }
 
 func isCompleteTree(root *TreeNode) bool {
 	if root == nil{
 		return true
 	}
-	var record map[int][]int = make(map[int][]int)
-	visit_isCompleteTree(root,0,1,record)
-	var total int = 0
-	var max_num int = 0
-	for _,nums := range record{
-		for _,n := range nums{
-			total++
-			if n > max_num{
-				max_num = n
+	var q list.List
+	q.PushBack(root)
+	var empty_child bool = false
+	for q.Len() > 0{
+		var l int = q.Len()
+		for i := 0;i < l;i++{
+			var cur *TreeNode = q.Front().Value.(*TreeNode)
+			q.Remove(q.Front())
+			if cur.Left == nil && cur.Right != nil{
+				return false
+			}
+			if !empty_child{
+				if cur.Left == nil || cur.Right == nil{ //都为空，右边为空
+					empty_child = true
+				}
+				if cur.Left != nil{
+					q.PushBack(cur.Left)
+				}
+				if cur.Right != nil{
+					q.PushBack(cur.Right)
+				}
+			}else{
+				if cur.Left != nil || cur.Right != nil{
+					return false
+				}
 			}
 		}
 	}
-	return total == max_num
+	return true
 }
+
+
+
+//func visit_isCompleteTree(node *TreeNode,parent int,cur int,record map[int][]int){
+//	if node == nil{
+//		return
+//	}
+//	record[parent] = append(record[parent],cur)
+//	visit_isCompleteTree(node.Left,cur,cur * 2,record)
+//	visit_isCompleteTree(node.Right,cur,cur * 2 + 1,record)
+//}
+//
+//func isCompleteTree(root *TreeNode) bool {
+//	if root == nil{
+//		return true
+//	}
+//	var record map[int][]int = make(map[int][]int)
+//	visit_isCompleteTree(root,0,1,record)
+//	var total int = 0
+//	var max_num int = 0
+//	for _,nums := range record{
+//		for _,n := range nums{
+//			total++
+//			if n > max_num{
+//				max_num = n
+//			}
+//		}
+//	}
+//	return total == max_num
+//}
