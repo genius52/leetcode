4386dc76a417d0c2a13096b134f367bad35d41a5
genius52 <genius52@qq.com>
2021-07-23 13:58:35 +0800

    977 add golang solution by two pointer

---
+++ src/golang/array/977. Squares of a Sorted Array.go
@@ -0,0 +1,28 @@
+package array
+
+func sortedSquares(nums []int) []int {
+	var l int = len(nums)
+	var res []int = make([]int,l)
+	var low int = 0
+	var high int = l - 1
+	var index int = l - 1
+	for low <= high{
+		if nums[low] >= 0{
+			break
+		}
+		if nums[high] > -nums[low]{
+			res[index] = nums[high] * nums[high]
+			high--
+		}else{
+			res[index] = nums[low] * nums[low]
+			low++
+		}
+		index--
+	}
+	for low <= high{
+		res[index] = nums[high] * nums[high]
+		high--
+		index--
+	}
+	return res
+}
