debc5de4f32ce5a2d62b4e9d7c27ab3743d40f81
genius52 <genius52@qq.com>
2021-08-09 09:23:47 +0800

    1041 optimize logic

--- src/golang/diagram/1041. Robot Bounded In Circle.go
+++ src/golang/diagram/1041. Robot Bounded In Circle.go
@@ -7,86 +7,107 @@ package diagram
 //Output: true
 //Explanation:
 //The robot moves from (0, 0) -> (0, 1) -> (-1, 1) -> (-1, 0) -> (0, 0) -> ...
-func IsRobotBounded(instructions string) bool {
+func IsRobotBounded(instructions string) bool{
 	var l int = len(instructions)
-	var cur_x int = 0
-	var cur_y int = 0
-	var dir []int = []int{0,1}
+	var dirs [][]int = [][]int{{0,1},{1,0},{0,-1},{-1,0}}
+	var idx int = 0
+	x := 0
+	y := 0
 	for i := 0;i < l;i++{
 		if instructions[i] == 'G'{
-			cur_x += dir[0]
-			cur_y += dir[1]
+			x += dirs[idx][0]
+			y += dirs[idx][1]
 		}else if instructions[i] == 'L'{
-			if dir[0] == 0 && dir[1] == 1{
-				dir[0] = -1
-				dir[1] = 0
-			}else if dir[0] == -1 && dir[1] == 0{
-				dir[0] = 0
-				dir[1] = -1
-			}else if dir[0] == 0 && dir[1] == -1{
-				dir[0] = 1
-				dir[1] = 0
-			}else if dir[0] == 1 && dir[1] == 0{
-				dir[0] = 0
-				dir[1] = 1
-			}
+			idx = (idx + 4 - 1) % 4
 		}else if instructions[i] == 'R'{
-			if dir[0] == 0 && dir[1] == 1{
-				dir[0] = 1
-				dir[1] = 0
-			}else if dir[0] == 1 && dir[1] == 0{
-				dir[0] = 0
-				dir[1] = -1
-			}else if dir[0] == 0 && dir[1] == -1{
-				dir[0] = -1
-				dir[1] = 0
-			}else if dir[0] == -1 && dir[1] == 0{
-				dir[0] = 0
-				dir[1] = 1
-			}
+			idx = (idx + 1) % 4
 		}
 	}
-	if cur_x == 0 && cur_y == 0{
+	if x == 0 && y == 0{
 		return true
 	}
-	for round := 0;round < 3;round++{
-		for i := 0;i < l;i++{
-			if instructions[i] == 'G'{
-				cur_x += dir[0]
-				cur_y += dir[1]
-			}else if instructions[i] == 'L'{
-				if dir[0] == 0 && dir[1] == 1{
-					dir[0] = -1
-					dir[1] = 0
-				}else if dir[0] == -1 && dir[1] == 0{
-					dir[0] = 0
-					dir[1] = -1
-				}else if dir[0] == 0 && dir[1] == -1{
-					dir[0] = 1
-					dir[1] = 0
-				}else if dir[0] == 1 && dir[1] == 0{
-					dir[0] = 0
-					dir[1] = 1
-				}
-			}else if instructions[i] == 'R'{
-				if dir[0] == 0 && dir[1] == 1{
-					dir[0] = 1
-					dir[1] = 0
-				}else if dir[0] == 1 && dir[1] == 0{
-					dir[0] = 0
-					dir[1] = -1
-				}else if dir[0] == 0 && dir[1] == -1{
-					dir[0] = -1
-					dir[1] = 0
-				}else if dir[0] == -1 && dir[1] == 0{
-					dir[0] = 0
-					dir[1] = 1
-				}
-			}
-		}
-	}
-	if cur_x == 0 && cur_y == 0{
-		return true
-	}
-	return false
+	return idx != 0
 }
+//func IsRobotBounded(instructions string) bool {
+//	var l int = len(instructions)
+//	var cur_x int = 0
+//	var cur_y int = 0
+//	var dir []int = []int{0,1}
+//	for i := 0;i < l;i++{
+//		if instructions[i] == 'G'{
+//			cur_x += dir[0]
+//			cur_y += dir[1]
+//		}else if instructions[i] == 'L'{
+//			if dir[0] == 0 && dir[1] == 1{
+//				dir[0] = -1
+//				dir[1] = 0
+//			}else if dir[0] == -1 && dir[1] == 0{
+//				dir[0] = 0
+//				dir[1] = -1
+//			}else if dir[0] == 0 && dir[1] == -1{
+//				dir[0] = 1
+//				dir[1] = 0
+//			}else if dir[0] == 1 && dir[1] == 0{
+//				dir[0] = 0
+//				dir[1] = 1
+//			}
+//		}else if instructions[i] == 'R'{
+//			if dir[0] == 0 && dir[1] == 1{
+//				dir[0] = 1
+//				dir[1] = 0
+//			}else if dir[0] == 1 && dir[1] == 0{
+//				dir[0] = 0
+//				dir[1] = -1
+//			}else if dir[0] == 0 && dir[1] == -1{
+//				dir[0] = -1
+//				dir[1] = 0
+//			}else if dir[0] == -1 && dir[1] == 0{
+//				dir[0] = 0
+//				dir[1] = 1
+//			}
+//		}
+//	}
+//	if cur_x == 0 && cur_y == 0{
+//		return true
+//	}
+//	for round := 0;round < 3;round++{
+//		for i := 0;i < l;i++{
+//			if instructions[i] == 'G'{
+//				cur_x += dir[0]
+//				cur_y += dir[1]
+//			}else if instructions[i] == 'L'{
+//				if dir[0] == 0 && dir[1] == 1{
+//					dir[0] = -1
+//					dir[1] = 0
+//				}else if dir[0] == -1 && dir[1] == 0{
+//					dir[0] = 0
+//					dir[1] = -1
+//				}else if dir[0] == 0 && dir[1] == -1{
+//					dir[0] = 1
+//					dir[1] = 0
+//				}else if dir[0] == 1 && dir[1] == 0{
+//					dir[0] = 0
+//					dir[1] = 1
+//				}
+//			}else if instructions[i] == 'R'{
+//				if dir[0] == 0 && dir[1] == 1{
+//					dir[0] = 1
+//					dir[1] = 0
+//				}else if dir[0] == 1 && dir[1] == 0{
+//					dir[0] = 0
+//					dir[1] = -1
+//				}else if dir[0] == 0 && dir[1] == -1{
+//					dir[0] = -1
+//					dir[1] = 0
+//				}else if dir[0] == -1 && dir[1] == 0{
+//					dir[0] = 0
+//					dir[1] = 1
+//				}
+//			}
+//		}
+//	}
+//	if cur_x == 0 && cur_y == 0{
+//		return true
+//	}
+//	return false
+//}
