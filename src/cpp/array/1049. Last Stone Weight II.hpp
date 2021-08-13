7b164c4d1ada975b05e4117f9865c99378a0e887
genius52 <genius52@qq.com>
2021-08-13 10:53:29 +0800

    1049 optimize by priority_queue

--- src/cpp/array/1049. Last Stone Weight II.hpp
+++ src/cpp/array/1049. Last Stone Weight II.hpp
@@ -5,6 +5,26 @@ using namespace std;
 
 class Solution_1049 {
 public:
+    int lastStoneWeight(vector<int>& stones){
+        int len = stones.size();
+        std::priority_queue<int> q;
+        for(int i = 0;i < len;i++){
+            q.push(stones[i]);
+        }
+        while(q.size() > 1){
+            auto top1 = q.top();
+            q.pop();
+            auto top2 = q.top();
+            q.pop();
+            auto diff = abs(top1 - top2);
+            if(diff != 0)
+                q.push(diff);
+        }
+        if(q.size() == 0)
+            return 0;
+        return q.top();
+    }
+    
     //1 <= stones.length <= 30
     //1 <= stones[i] <= 100
     int lastStoneWeightII(vector<int>& stones){
