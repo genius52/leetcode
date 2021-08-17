f9f5fd5a175ed8df80e344d3a58cf6cde99d88c5
genius52 <genius52@qq.com>
2021-08-17 09:38:31 +0800

    1072 move to single file

--- src/golang/array/array_practice.go
+++ src/golang/array/array_practice.go
@@ -680,40 +680,6 @@ func shiftGrid(grid [][]int, k int) [][]int {
 	return grid2
 }
 
-//1072
-func maxEqualRowsAfterFlips(matrix [][]int) int {
-	var res int = 0
-	var record map[string]int = make(map[string]int)
-	for row := 0; row < len(matrix);row++{
-		var s string = ""
-		var reverse_s string = ""
-		for column := 0; column < len(matrix[0]);column++{
-			if matrix[row][column] == 1{
-				reverse_s += "0"
-			}else{
-				reverse_s += "1"
-			}
-			s += strconv.Itoa(matrix[row][column])
-		}
-		if _,ok := record[reverse_s];ok{
-			record[reverse_s]++
-		}else{
-			record[reverse_s] = 1
-		}
-		if _,ok := record[s];ok{
-			record[s]++
-		}else{
-			record[s] = 1
-		}
-	}
-	for _,val := range record{
-		if val > res{
-			res = val
-		}
-	}
-	return res
-}
-
 //1249
 // "())()((("
 func reverse(s string) string {
