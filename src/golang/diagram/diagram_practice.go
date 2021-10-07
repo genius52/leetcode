91bb9d37baa56c4173e7d0bd349d5440561d57cc
genius52 <genius52@qq.com>
2021-10-07 09:31:50 +0800

    1392 move to single file

--- src/golang/diagram/diagram_practice.go
+++ src/golang/diagram/diagram_practice.go
@@ -204,29 +204,6 @@ func isMatch(s string, p string) bool {
 	return dp_isMatch(s,p,0,0,0)
 }
 
-//1392
-//Input: s = "ababab"
-//Output: "abab"
-func longestPrefix(s string) string {
-	l := len(s)
-	var mod int = 10e9 + 7
-	var res string
-	var head int = 0
-	var tail int = 0
-	var times int = 1
-	for i := 0;i < l - 1;i++{
-		var s1 int = int((s[i] - 'a') * 26)
-		var s2 int = int((s[l - 1 - i] - 'a') * 26)
-		head = (head * 26 + s1) % mod
-		tail = (s2 * times + tail) % mod
-		times = times * 26 % mod
-		if head == tail{
-			res = s[:i+1]
-		}
-	}
-	return res
-}
-
 //1284
 func minFlips(mat [][]int) int {
 	//rows := len(mat)
