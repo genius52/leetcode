56f38e1a2988c30acc628e84941751013009c60e
genius52 <genius52@qq.com>
2021-10-20 08:24:03 +0800

    1460 add golang solution

---
+++ src/golang/array/1460. Make Two Arrays Equal by Reversing Sub-arrays.go
@@ -0,0 +1,14 @@
+package array
+
+import "sort"
+
+func canBeEqual(target []int, arr []int) bool {
+	sort.Ints(target)
+	sort.Ints(arr)
+	for i := 0;i < len(target);i++{
+		if target[i] != arr[i]{
+			return false
+		}
+	}
+	return true
+}
