1313e3758ff0e74dd4cf9846eaa8add66c278d87
genius52 <genius52@qq.com>
2021-10-27 10:57:41 +0800

    1496 optimize by store pair in std::set

--- src/cpp/graph/1496. Path Crossing.hpp
+++ src/cpp/graph/1496. Path Crossing.hpp
@@ -4,26 +4,46 @@ using namespace std;
 
 class Solution_1496 {
 public:
-    bool isPathCrossing(string path) {
-        std::unordered_set<std::string> s;
-        s.insert("0,0");
-        int horizon = 0;
-        int vertical = 0;
-        int len = path.length();
+    bool isPathCrossing(string path){
+        std::set<std::pair<int,int>> trace;
+        trace.insert({0,0});
+        int x,y = 0;
+        int len = path.size();
         for(int i = 0;i < len;i++){
             if(path[i] == 'N')
-                horizon++;
+                y += 1;
             else if(path[i] == 'S')
-                horizon--;
+                y -= 1;
             else if(path[i] == 'E')
-                vertical++;
+                x += 1;
             else if(path[i] == 'W')
-                vertical--;
-            std::string v =  std::to_string(horizon) + "," + std::to_string(vertical);
-            if(s.find(v) != s.end())
+                x -= 1;
+            if(trace.find({x,y}) != trace.end())
                 return true;
-            s.insert(v);
+            trace.insert({x,y});
         }
         return false;
     }
+//    bool isPathCrossing(string path) {
+//        std::unordered_set<std::string> s;
+//        s.insert("0,0");
+//        int horizon = 0;
+//        int vertical = 0;
+//        int len = path.length();
+//        for(int i = 0;i < len;i++){
+//            if(path[i] == 'N')
+//                horizon++;
+//            else if(path[i] == 'S')
+//                horizon--;
+//            else if(path[i] == 'E')
+//                vertical++;
+//            else if(path[i] == 'W')
+//                vertical--;
+//            std::string v =  std::to_string(horizon) + "," + std::to_string(vertical);
+//            if(s.find(v) != s.end())
+//                return true;
+//            s.insert(v);
+//        }
+//        return false;
+//    }
 };
