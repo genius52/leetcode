811a403b9b3b20cee55fcd9e405bdd7a2f80f5ea
genius52 <genius52@qq.com>
2021-11-09 22:02:35 +0800

    1576 make code clear

--- src/golang/string_issue/1576. Replace All to Avoid Consecutive Repeating Characters.go
+++ src/golang/string_issue/1576. Replace All to Avoid Consecutive Repeating Characters.go
@@ -2,6 +2,25 @@ package string_issue
 
 //Input: s = "??yw?ipkj?"
 //Output: "acywaipkja"
+func modifyString(s string) string {
+	var l int = len(s)
+	backup := [3]uint8{'a','b','c'}
+	var res string
+	for i := 0;i < l;i++{
+		if s[i] != '?'{
+			res += string(s[i])
+		}else{
+			for j := 0;j < 3;j++{
+				if (i == 0 && i == l - 1) || (i == 0 && backup[j] != s[i + 1]) || (i == l - 1 && backup[j] != res[i - 1]) || (i > 0 && i < l - 1 && backup[j] != res[i - 1] && backup[j] != s[i + 1]){
+					res += string(backup[j])
+					break
+				}
+			}
+		}
+	}
+	return res
+}
+
 func ModifyString(s string) string {
 	var res string
 	s = "0" + s
