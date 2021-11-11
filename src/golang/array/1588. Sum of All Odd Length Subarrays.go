746a2d9bedd1ecd82e1eb473ad8ada1f68232374
genius52 <genius52@qq.com>
2021-11-11 15:51:39 +0800

    1588 optimized by (i + 1) * (l - i)

--- src/golang/array/1588. Sum of All Odd Length Subarrays.go
+++ src/golang/array/1588. Sum of All Odd Length Subarrays.go
@@ -12,6 +12,20 @@ package array
 //[4,2,5] = 11
 //[2,5,3] = 10
 //[1,4,2,5,3] = 15
+//对于一个数字，它所在的数组，可以在它前面再选择 0, 1, 2, ... 个数字，一共有 left = i + 1 个选择；
+//
+//可以在它后面再选择 0, 1, 2, ... 个数字，一共有 right = n - i 个选择。
+func sumOddLengthSubarrays(arr []int) int{
+	var l int = len(arr)
+	var res int = 0
+	for i := 0;i < l;i++{
+		left_cnt := i + 1
+		right_cnt := l - i
+		res += ((left_cnt * right_cnt) + 1)/2 * arr[i]
+	}
+	return res
+}
+
 func SumOddLengthSubarrays(arr []int) int {
 	var l int = len(arr)
 	var res int = 0
