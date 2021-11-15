c07b6a80cb2d7a9799e9138a011c0a16be47de7a
genius52 <genius52@qq.com>
2021-11-15 16:09:53 +0800

    1609 add bfs solution

--- src/golang/tree/1609. Even Odd Tree.go
+++ src/golang/tree/1609. Even Odd Tree.go
@@ -1,5 +1,7 @@
 package tree
 
+import "container/list"
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -9,6 +11,7 @@ package tree
  * }
  */
 
+//recrusive solution
 func previsit_isEvenOddTree(node *TreeNode,level int,even_queue map[int][]int,odd_queue map[int][]int)bool{
 	if node == nil{
 		return true
@@ -51,4 +54,52 @@ func IsEvenOddTree(root *TreeNode) bool {
 	var even_queue map[int][]int = make(map[int][]int)
 	var odd_queue map[int][]int = make(map[int][]int)
 	return previsit_isEvenOddTree(root,0,even_queue,odd_queue)
+}
+
+//偶数下标 层上的所有节点的值都是 奇 整数，从左到右按顺序 严格递增
+//奇数下标 层上的所有节点的值都是 偶 整数，从左到右按顺序 严格递减
+//bfs solution
+func isEvenOddTree(root *TreeNode) bool{
+	var q list.List
+	even_level := true
+	q.PushBack(root)
+	for q.Len() > 0{
+		var l int = q.Len()
+		var pre *TreeNode = nil
+		for i := 0;i < l;i++{
+			cur := q.Front().Value.(*TreeNode)
+			q.Remove(q.Front())
+			if cur.Left != nil{
+				q.PushBack(cur.Left)
+			}
+			if cur.Right != nil{
+				q.PushBack(cur.Right)
+			}
+			if even_level{
+				if (cur.Val | 1) != cur.Val{
+					return false
+				}
+			}else{
+				if (cur.Val | 1) == cur.Val{
+					return false
+				}
+			}
+			if pre == nil{
+				pre = cur
+			}else{
+				if even_level{
+					if pre.Val >= cur.Val{
+						return false
+					}
+				}else{
+					if pre.Val <= cur.Val{
+						return false
+					}
+				}
+				pre = cur
+			}
+		}
+		even_level = !even_level
+	}
+	return true
 }
