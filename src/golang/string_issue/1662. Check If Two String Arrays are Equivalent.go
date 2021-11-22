4d0369b3a7f619eb381d952a725b085c8528640f
genius52 <genius52@qq.com>
2021-11-22 13:33:52 +0800

    1662 improve time and space performance

--- src/golang/string_issue/1662. Check If Two String Arrays are Equivalent.go
+++ src/golang/string_issue/1662. Check If Two String Arrays are Equivalent.go
@@ -7,3 +7,30 @@ func arrayStringsAreEqual(word1 []string, word2 []string) bool {
 	var s2 string = strings.Join(word2,"")
 	return s1 == s2
 }
+
+func arrayStringsAreEqual2(word1 []string, word2 []string) bool{
+	var i int = 0
+	var j int = 0
+	var visit1 int = 0
+	var visit2 int = 0
+	var l1 int = len(word1)
+	var l2 int = len(word2)
+	for i < l1 && j < l2{
+		for visit1 < len(word1[i]) && visit2 < len(word2[j]){
+			if word1[i][visit1] != word2[j][visit2]{
+				return false
+			}
+			visit1++
+			visit2++
+		}
+		if visit1 >= len(word1[i]){
+			i++
+			visit1 = 0
+		}
+		if visit2 >= len(word2[j]){
+			j++
+			visit2 = 0
+		}
+	}
+	return i == l1 && j == l2
+}
