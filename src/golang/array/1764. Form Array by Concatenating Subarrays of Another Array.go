e176adac8ee942ecc0b872038f6363f77d6d64bf
genius52 <genius52@qq.com>
2021-12-06 11:34:47 +0800

    1764 fix previous wrong dfs solution

--- src/golang/array/1764. Form Array by Concatenating Subarrays of Another Array.go
+++ src/golang/array/1764. Form Array by Concatenating Subarrays of Another Array.go
@@ -1,31 +1,36 @@
 package array
 
-func dfs_canChoose(groups [][]int,index int,nums []int,pos int)bool{
-	var group_len int = len(groups)
-	var nums_len int = len(nums)
-	if index >= group_len{
+//Input: groups = [[1,-1,-1],[3,-2,0]], nums = [1,-1,0,1,-1,-1,3,-2,0]
+//Output: true
+//Explanation: You can choose the 0th subarray as [1,-1,0,1,-1,-1,3,-2,0] and the 1st one as [1,-1,0,1,-1,-1,3,-2,0].
+func dfs_canChoose(groups [][]int, l1 int, idx1 int, nums []int, l2 int, idx2 int) bool {
+	if idx1 >= l1 {
 		return true
 	}
-	var sub_len = len(groups[index])
-	for i := 0;i < sub_len;i++{
-		if pos + i >= nums_len{
-			return false
-		}
-		if groups[index][i] != nums[pos + i]{
-			return false
-		}
+	if idx2 >= l2 {
+		return false
 	}
-	return dfs_canChoose(groups,index + 1,nums,pos + sub_len)
-}
-
-func CanChoose(groups [][]int, nums []int) bool {
-	var l int = len(nums)
-	var res bool = false
-	for i := 0;i < l;i++{
-		res = dfs_canChoose(groups,0,nums,i)
-		if res{
+	var i int = idx2
+	var j int = 0
+	var group_len int = len(groups[idx1])
+	for i < l2 && j < group_len {
+		if groups[idx1][j] == nums[i] {
+			i++
+			j++
+		} else {
 			break
 		}
 	}
-	return res
+	var res bool = false
+	if j == group_len {
+		res = dfs_canChoose(groups, l1, idx1+1, nums, l2, i)
+	}
+	if res {
+		return true
+	}
+	return dfs_canChoose(groups, l1, idx1, nums, l2, idx2+1)
+}
+
+func CanChoose(groups [][]int, nums []int) bool {
+	return dfs_canChoose(groups, len(groups), 0, nums, len(nums), 0)
 }
