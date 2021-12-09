377e7fe43bb0032e342c39b10b48a1b50006cad6
genius52 <genius52@qq.com>
2021-12-09 10:08:35 +0800

    1759 optimize code style

--- src/golang/string_issue/1758. Minimum Changes To Make Alternating Binary String.go
+++ src/golang/string_issue/1758. Minimum Changes To Make Alternating Binary String.go
@@ -1,34 +1,59 @@
 package string_issue
 
-func min_int(a,b int)int{
+func min_int(a, b int) int {
 	if a < b {
 		return a
-	}else{
+	} else {
 		return b
 	}
 }
 
+func minOperations(s string) int {
+	var l int = len(s)
+	var start_zero_opt_cnt int = 0 //010101
+	var start_one_opt_cnt int = 0  //101010
+	for i := 0; i < l; i++ {
+		if i|1 == i { //odd index
+			if s[i] == '0' {
+				start_zero_opt_cnt++
+			} else {
+				start_one_opt_cnt++
+			}
+		} else { //even index
+			if s[i] == '0' {
+				start_one_opt_cnt++
+			} else {
+				start_zero_opt_cnt++
+			}
+		}
+	}
+	if start_zero_opt_cnt < start_one_opt_cnt {
+		return start_zero_opt_cnt
+	}
+	return start_one_opt_cnt
+}
+
 func MinOperations(s string) int {
 	var l int = len(s)
-	if l <= 1{
+	if l <= 1 {
 		return 0
 	}
 	var zero_one int = 0
 	var one_zero int = 0
-	for i := 0;i < l;i++{
-		if (i | 1) == i{
-			if s[i] != '1'{
+	for i := 0; i < l; i++ {
+		if (i | 1) == i {
+			if s[i] != '1' {
 				zero_one++
-			}else{
+			} else {
 				one_zero++
 			}
-		}else{
-			if s[i] != '0'{
+		} else {
+			if s[i] != '0' {
 				zero_one++
-			}else{
+			} else {
 				one_zero++
 			}
 		}
 	}
-	return min_int(zero_one,one_zero)
+	return min_int(zero_one, one_zero)
 }
--- src/golang/string_issue/1759. Count Number of Homogenous Substrings.go
+++ src/golang/string_issue/1759. Count Number of Homogenous Substrings.go
@@ -11,23 +11,41 @@ package string_issue
 //"cc"  appears 2 times.
 //"ccc" appears 1 time.
 //3 + 1 + 2 + 1 + 3 + 2 + 1 = 13.
-func calculate(n int)int{
-	return n + n * (n - 1)/2
+func countHomogenous(s string) int {
+	var l int = len(s)
+	var res int = 0
+	var cnt int = 1
+	for i := 0; i < l; i++ {
+		if i > 0 {
+			if s[i] == s[i-1] {
+				cnt++
+			} else {
+				cnt = 1
+			}
+		}
+		res += cnt
+		res %= 1e9 + 7
+	}
+	return res
+}
+
+func calculate(n int) int {
+	return n + n*(n-1)/2
 }
 
 func CountHomogenous(s string) int {
 	var l int = len(s)
-	if l <= 1{
+	if l <= 1 {
 		return 1
 	}
 	s += "0"
 	var begin int = 0
 	var end int = 1
 	var res int = 0
-	for end <= l{
-		if s[end] == s[end - 1]{
+	for end <= l {
+		if s[end] == s[end-1] {
 			end++
-		}else{
+		} else {
 			res += calculate(end - begin)
 			res %= 1000000007
 			begin = end
