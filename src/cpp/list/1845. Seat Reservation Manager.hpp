e005b151b681ec85e1e69c7e5d4d33f254e30a54
genius52 <genius52@qq.com>
2021-12-25 12:46:00 +0800

    1845 add priority queue solution

--- src/cpp/list/1845. Seat Reservation Manager.hpp
+++ src/cpp/list/1845. Seat Reservation Manager.hpp
@@ -1,22 +1,43 @@
 #include <set>
+#include <queue>
 using namespace std;
 
 class SeatManager {
-    std::set<int> s;
+    std::priority_queue<int,std::vector<int>,std::greater<int>> q_;
 public:
     SeatManager(int n) {
         for(int i = 1;i <= n;i++){
-            s.insert(i);
+            q_.push(i);
         }
     }
 
     int reserve() {
-        auto res = *(s.begin());
-        s.erase(s.begin());
+        int res = q_.top();
+        q_.pop();
         return res;
     }
 
     void unreserve(int seatNumber) {
-        s.insert(seatNumber);
+        q_.push(seatNumber);
     }
 };
+
+//class SeatManager {
+//    std::set<int> s;
+//public:
+//    SeatManager(int n) {
+//        for(int i = 1;i <= n;i++){
+//            s.insert(i);
+//        }
+//    }
+//
+//    int reserve() {
+//        auto res = *(s.begin());
+//        s.erase(s.begin());
+//        return res;
+//    }
+//
+//    void unreserve(int seatNumber) {
+//        s.insert(seatNumber);
+//    }
+//};
