edfdbfeace80b7e5630eb13837807e269cc7545a
genius52 <genius52@qq.com>
2022-01-07 09:00:12 +0800

    1903 optimize code logic

--- src/golang/number/1903. Largest Odd Number in String.go
+++ src/golang/number/1903. Largest Odd Number in String.go
@@ -5,14 +5,25 @@ import "strconv"
 func LargestOddNumber(num string) string {
 	var l int = len(num)
 	i := l - 1
-	for ;i >= 0;i--{
-		val,_ := strconv.Atoi(string(num[i]))
-		if (val | 1) == val{
+	for ; i >= 0; i-- {
+		val, _ := strconv.Atoi(string(num[i]))
+		if (val | 1) == val {
 			break
 		}
 	}
-	if i == -1{
+	if i == -1 {
 		return ""
 	}
-	return num[:i + 1]
+	return num[:i+1]
+}
+
+func largestOddNumber(num string) string {
+	var l int = len(num)
+	for i := l - 1; i >= 0; i-- {
+		n := num[i] - '0'
+		if n|1 == n {
+			return num[:i+1]
+		}
+	}
+	return ""
 }
