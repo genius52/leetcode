82c11500ec6fb2fe64f8fcf65386c50f71be5469
genius52 <genius52@qq.com>
2022-01-14 22:06:52 +0800

    1913 fix compile error

--- src/golang/number/1913. Maximum Product Difference Between Two Pairs.go
+++ src/golang/number/1913. Maximum Product Difference Between Two Pairs.go
@@ -7,10 +7,3 @@ func maxProductDifference(nums []int) int {
 	var l int = len(nums)
 	return nums[l-1]*nums[l-2] - nums[0]*nums[1]
 }
-
-func maxProductDifference2(nums []int) int {
-	sort.Ints(nums)
-	var l int = len(nums)
-	var small1 smal
-	return nums[l-1]*nums[l-2] - nums[0]*nums[1]
-}
