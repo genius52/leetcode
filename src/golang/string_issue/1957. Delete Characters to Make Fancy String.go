92ff3d6e1c42d8a8d69b9356990acd21cd75c089
genius52 <genius52@qq.com>
2022-01-24 14:30:08 +0800

    1957 optimize by stringbuilder

--- src/golang/string_issue/1957. Delete Characters to Make Fancy String.go
+++ src/golang/string_issue/1957. Delete Characters to Make Fancy String.go
@@ -1,5 +1,27 @@
 package string_issue
 
+import "strings"
+
+func makeFancyString2(s string) string{
+	var l int = len(s)
+	var sb strings.Builder
+	var left int = 0
+	for left < l{
+		right := left + 1
+		for right < l && s[left] == s[right]{
+			right++
+		}
+		cur_len := right - left
+		if cur_len <= 2{
+			sb.WriteString(s[left:right])
+		}else{
+			sb.WriteString(s[left:left + 2])
+		}
+		left = right
+	}
+	return sb.String()
+}
+
 func makeFancyString(s string) string {
 	var l int = len(s)
 	var left int = 0
