f1eb93d8498e58a6aa4903beede9c1c7294c153f
genius52 <genius52@qq.com>
2022-01-27 20:53:19 +0800

    1980 Tie solution

--- src/golang/string_issue/1980. Find Unique Binary String.go
+++ src/golang/string_issue/1980. Find Unique Binary String.go
@@ -35,3 +35,57 @@ func convertToBin(num int,target_len int) string {
 	}
 	return s
 }
+
+type Tie_binary struct{
+	next [2]*Tie_binary
+	//val int
+}
+
+func dfs_findDifferentBinaryString(tie *Tie_binary,l int)string{
+	if tie == nil{
+
+	}
+	if tie.next[0] == nil{
+		var s string
+		for i := 0;i < l;i++{
+			s += "0"
+		}
+		return s
+	}else if tie.next[1] == nil{
+		var s string
+		for i := 0;i < l;i++{
+			s += "1"
+		}
+		return s
+	}
+	zero_s := dfs_findDifferentBinaryString(tie.next[0],l - 1)
+	one_s := dfs_findDifferentBinaryString(tie.next[1],l - 1)
+	if len(zero_s) != 0{
+		return "0" + zero_s
+	}else{
+		return "1" + one_s
+	}
+}
+
+func findDifferentBinaryString2(nums []string) string{
+	var l int = len(nums[0])
+	var root Tie_binary
+	for _,num := range nums{
+		var visit *Tie_binary = &root
+		for i := 0;i < l;i++{
+			if num[i] == '0'{
+				if visit.next[0] == nil{
+					visit.next[0] = new(Tie_binary)
+				}
+				visit = visit.next[0]
+			}else{
+				if visit.next[1] == nil{
+					visit.next[1] = new(Tie_binary)
+				}
+				visit = visit.next[1]
+			}
+		}
+	}
+	res := dfs_findDifferentBinaryString(&root,l)
+	return res
+}
