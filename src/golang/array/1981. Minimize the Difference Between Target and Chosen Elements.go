74f7530e32caa37359e5f257fee609a62e1f815b
genius52 <genius52@qq.com>
2022-01-28 09:55:24 +0800

    1981 optimize time performance

--- src/golang/array/1981. Minimize the Difference Between Target and Chosen Elements.go
+++ src/golang/array/1981. Minimize the Difference Between Target and Chosen Elements.go
@@ -2,80 +2,109 @@ package array
 
 import "sort"
 
-func abs_diff(a int,b int)int{
-	if a > b{
-		return a - b
-	}else{
-		return b - a
-	}
-}
-
-func dp_minimizeTheDifference(mat *[][]int,rows int,columns int,r int,choose *[]int,cur_sum int,target int,memo *map[int]map[int]int)(int,int){
-	if cur_sum == target{
-		return 0,cur_sum
-	}
+func dp_minimizeTheDifference(mat [][]int,rows int,columns int,r int,sum int,target int,res *int,memo [][5000]bool){
 	if r >= rows{
-		return abs_diff(cur_sum,target),cur_sum
-	}
-	keys := r * 100 + (*choose)[r]
-	if _,ok1 := (*memo)[keys];ok1{
-		if _,ok2 := (*memo)[keys][cur_sum];ok2{
-			return (*memo)[keys][cur_sum],cur_sum
+		cur := abs_int(sum - target)
+		if cur < *res{
+			*res = cur
 		}
+		return
 	}
-	//skip current row
-	res1,sum1 := dp_minimizeTheDifference(mat,rows,columns,r + 1,choose,cur_sum,target,memo)
-	//move current row to right if available
-
-	var res2 int = 2147483647
-	var sum2 int = 0
-	if ((*choose)[r] + 1) < columns{
-		for i := 1;i < columns && ((*choose)[r] + i) < columns;i++{
-			(*choose)[r] += i
-			cur,s2 := dp_minimizeTheDifference(mat,rows,columns,r + 1,choose,cur_sum + (*mat)[r][(*choose)[r]] - (*mat)[r][(*choose)[r] - i],target,memo)
-			(*choose)[r] -= i
-			if cur < res2{
-				res2 = cur
-				sum2 = s2
-			}else{
-				if s2 > target{
-					break
-				}
-			}
-		}
+	if memo[r][sum] || sum - target >= *res{
+		return
 	}
-	if _,ok := (*memo)[keys];!ok{
-		(*memo)[keys] = make(map[int]int)
-	}
-	if res1 < res2{
-		(*memo)[keys][cur_sum] = res1
-		return res1,sum1
-	}else{
-		(*memo)[keys][cur_sum] = res2
-		return res2,sum2
+	memo[r][sum] = true
+	for j := 0;j < columns;j++{
+		dp_minimizeTheDifference(mat,rows,columns,r + 1,sum + mat[r][j],target,res,memo)
 	}
 }
 
-func minimizeTheDifference(mat [][]int, target int) int {
-	var rows int = len(mat)
-	var columns int = len(mat[0])
+func minimizeTheDifference(mat [][]int, target int) int{
+	rows := len(mat)
+	columns := len(mat[0])
 	for i := 0;i < rows;i++{
 		sort.Ints(mat[i])
 	}
-	low := 0
-	high := 0
-	for i := 0;i < rows;i++{
-		low += mat[i][0]
-		high += mat[i][columns - 1]
-	}
-	if low >= target{
-		return low - target
-	}
-	if high <= target{
-		return target - high
-	}
-	var choose []int = make([]int,rows)
-	var memo map[int]map[int]int = make(map[int]map[int]int)//rows * 100 + columns,sum
-	res,_ := dp_minimizeTheDifference(&mat,rows,columns,0,&choose,low,target,&memo)
+	var memo [][5000]bool = make([][5000]bool,rows)
+	var res int = 2147483647
+	dp_minimizeTheDifference(mat,rows,columns,0,0,target,&res,memo)
 	return res
 }
+
+//func abs_diff(a int,b int)int{
+//	if a > b{
+//		return a - b
+//	}else{
+//		return b - a
+//	}
+//}
+
+//func dp_minimizeTheDifference(mat *[][]int,rows int,columns int,r int,choose *[]int,cur_sum int,target int,memo *map[int]map[int]int)(int,int){
+//	if cur_sum == target{
+//		return 0,cur_sum
+//	}
+//	if r >= rows{
+//		return abs_diff(cur_sum,target),cur_sum
+//	}
+//	keys := r * 100 + (*choose)[r]
+//	if _,ok1 := (*memo)[keys];ok1{
+//		if _,ok2 := (*memo)[keys][cur_sum];ok2{
+//			return (*memo)[keys][cur_sum],cur_sum
+//		}
+//	}
+//	//skip current row
+//	res1,sum1 := dp_minimizeTheDifference(mat,rows,columns,r + 1,choose,cur_sum,target,memo)
+//	//move current row to right if available
+//
+//	var res2 int = 2147483647
+//	var sum2 int = 0
+//	if ((*choose)[r] + 1) < columns{
+//		for i := 1;i < columns && ((*choose)[r] + i) < columns;i++{
+//			(*choose)[r] += i
+//			cur,s2 := dp_minimizeTheDifference(mat,rows,columns,r + 1,choose,cur_sum + (*mat)[r][(*choose)[r]] - (*mat)[r][(*choose)[r] - i],target,memo)
+//			(*choose)[r] -= i
+//			if cur < res2{
+//				res2 = cur
+//				sum2 = s2
+//			}else{
+//				if s2 > target{
+//					break
+//				}
+//			}
+//		}
+//	}
+//	if _,ok := (*memo)[keys];!ok{
+//		(*memo)[keys] = make(map[int]int)
+//	}
+//	if res1 < res2{
+//		(*memo)[keys][cur_sum] = res1
+//		return res1,sum1
+//	}else{
+//		(*memo)[keys][cur_sum] = res2
+//		return res2,sum2
+//	}
+//}
+//
+//func minimizeTheDifference(mat [][]int, target int) int {
+//	var rows int = len(mat)
+//	var columns int = len(mat[0])
+//	for i := 0;i < rows;i++{
+//		sort.Ints(mat[i])
+//	}
+//	low := 0
+//	high := 0
+//	for i := 0;i < rows;i++{
+//		low += mat[i][0]
+//		high += mat[i][columns - 1]
+//	}
+//	if low >= target{
+//		return low - target
+//	}
+//	if high <= target{
+//		return target - high
+//	}
+//	var choose []int = make([]int,rows)
+//	var memo map[int]map[int]int = make(map[int]map[int]int)//rows * 100 + columns,sum
+//	res,_ := dp_minimizeTheDifference(&mat,rows,columns,0,&choose,low,target,&memo)
+//	return res
+//}
