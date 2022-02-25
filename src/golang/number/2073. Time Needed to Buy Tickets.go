e7ff2195686a5691c2f907ecfabec2e34b108530
genius52 <genius52@qq.com>
2022-02-25 16:43:37 +0800

    2073 optimize code performance

--- src/golang/number/2073. Time Needed to Buy Tickets.go
+++ src/golang/number/2073. Time Needed to Buy Tickets.go
@@ -1,5 +1,23 @@
 package number
 
+func timeRequiredToBuy2(tickets []int, k int) int{
+	target := tickets[k]
+	var l int = len(tickets)
+	var res int = 0
+	for i := 0;i < l;i++{
+		if tickets[i] < target{
+			res += tickets[i]
+		}else{
+			if i <= k{
+				res += target
+			}else {
+				res += target - 1
+			}
+		}
+	}
+	return res
+}
+
 func timeRequiredToBuy(tickets []int, k int) int {
 	var l int = len(tickets)
 	cnt := tickets[k]
