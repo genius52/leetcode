a50fe81029df914dfa3c5508945bb086d60e43ca
genius52 <genius52@qq.com>
2022-03-07 09:22:34 +0800

    2102 using two heap

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,6 +6,32 @@
 
 int main() {
     {
+        SORTracker tracker;
+        tracker.add("bradford", 2); // 添加 name="bradford" 且 score=2 的景点。
+        tracker.add("branford", 3); // 添加 name="branford" 且 score=3 的景点。
+        auto res = tracker.get();              // 从好带坏的景点为：branford ，bradford 。
+        // 注意到 branford 比 bradford 好，因为它的 评分更高 (3 > 2) 。
+        // 这是第 1 次调用 get() ，所以返回最好的景点："branford" 。
+        tracker.add("alps", 2);     // 添加 name="alps" 且 score=2 的景点。
+        res = tracker.get();              // 从好到坏的景点为：branford, alps, bradford 。
+        // 注意 alps 比 bradford 好，虽然它们评分相同，都为 2 。
+        // 这是因为 "alps" 字典序 比 "bradford" 小。
+        // 返回第 2 好的地点 "alps" ，因为当前为第 2 次调用 get() 。
+        tracker.add("orland", 2);   // 添加 name="orland" 且 score=2 的景点。
+        res = tracker.get();              // 从好到坏的景点为：branford, alps, bradford, orland 。
+        // 返回 "bradford" ，因为当前为第 3 次调用 get() 。
+        tracker.add("orlando", 3);  // 添加 name="orlando" 且 score=3 的景点。
+        res = tracker.get();              // 从好到坏的景点为：branford, orlando, alps, bradford, orland 。
+        // 返回 "bradford".
+        tracker.add("alpine", 2);   // 添加 name="alpine" 且 score=2 的景点。
+        res = tracker.get();              // 从好到坏的景点为：branford, orlando, alpine, alps, bradford, orland 。
+        // 返回 "bradford" 。
+        res = tracker.get();              // 从好到坏的景点为：branford, orlando, alpine, alps, bradford, orland 。
+        // 返回 "orland" 。
+
+        std::cout<<"2102 res = "<<res<<std::endl;
+    }
+    {
         //s = "**|**|***|", queries = [[2,5],[5,9]]
         Solution_2055 s2055;
         std::string s = "**|**|***|";
