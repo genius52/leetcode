ef87f562dcf12895d58c2d64252f9cf8f80a7be7
genius52 <genius52@qq.com>
2022-03-24 15:52:47 +0800

    217 add kadane solution

--- src/golang/array/53. Maximum Subarray.go
+++ src/golang/array/53. Maximum Subarray.go
@@ -3,14 +3,26 @@ package array
 //Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 //Output: 6
 //Explanation: [4,-1,2,1] has the largest sum = 6.
+//kadane
 func MaxSubArray(nums []int) int{
 	var l int = len(nums)
-	var dp []int = make([]int,l)
-	dp[0] = nums[0]
-	var max_val int = nums[0]
+	var global_max int = nums[0]
+	var cur_max int = nums[0]
 	for i := 1;i < l;i++{
-		dp[i] = max_int(nums[i],dp[i - 1] + nums[i])
-		max_val = max_int(max_val,dp[i])
+		cur_max = max_int(cur_max + nums[i],nums[i])
+		global_max = max_int(global_max,cur_max)
 	}
-	return max_val
+	return global_max
 }
+
+//func MaxSubArray(nums []int) int{
+//	var l int = len(nums)
+//	var dp []int = make([]int,l)
+//	dp[0] = nums[0]
+//	var max_val int = nums[0]
+//	for i := 1;i < l;i++{
+//		dp[i] = max_int(nums[i],dp[i - 1] + nums[i])
+//		max_val = max_int(max_val,dp[i])
+//	}
+//	return max_val
+//}
--- src/golang/number/217. Contains Duplicate.go
+++ src/golang/number/217. Contains Duplicate.go
@@ -1,12 +1,25 @@
 package number
 
-func containsDuplicate(nums []int) bool {
-	var record map[int]bool = make(map[int]bool)
-	for _,n := range nums{
-		if _,ok := record[n];ok{
+import "sort"
+
+func containsDuplicate(nums []int) bool{
+	sort.Ints(nums)
+	var l int = len(nums)
+	for i := 1;i < l;i++{
+		if nums[i] == nums[i - 1]{
 			return true
 		}
-		record[n] = true
 	}
 	return false
 }
+
+//func containsDuplicate(nums []int) bool {
+//	var record map[int]bool = make(map[int]bool)
+//	for _,n := range nums{
+//		if _,ok := record[n];ok{
+//			return true
+//		}
+//		record[n] = true
+//	}
+//	return false
+//}
