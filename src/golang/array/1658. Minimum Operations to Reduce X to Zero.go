56e5f4735280854ceb4b72699a03ec52902f9336
genius52 <genius52@qq.com>
2022-06-11 20:21:05 +0800

    1658 add two pointer solution

--- src/golang/array/1658. Minimum Operations to Reduce X to Zero.go
+++ src/golang/array/1658. Minimum Operations to Reduce X to Zero.go
@@ -1,69 +1,101 @@
 package array
 
-import "math"
-
 func MinOperations1658(nums []int, x int) int{
 	var l int = len(nums)
 	var total int = 0
-	for i := 0;i < l;i++{
-		total += nums[i]
+	for _,n := range nums{
+		total += n
+	}
+	if total == x{
+		return l
+	}
+	if total < x{
+		return -1
 	}
 	var target int = total - x
-	if target < 0{
+	var left int = 0
+	var right int = 0
+	var max_len int = -1
+	var cur_sum int = 0
+	for left < l{
+		for right < l && cur_sum < target{
+			cur_sum += nums[right]
+			right++
+		}
+		if cur_sum == target{
+			max_len = max_int(max_len,right - left)
+		}
+		cur_sum -= nums[left]
+		left++
+	}
+	if max_len == -1{
 		return -1
 	}
-	i := 0
-	j := 0
-	var sum int = 0
-	var steps1 int = math.MaxInt32
-	for i < l && j < l{
-		for j < l && sum < target{
-			sum += nums[j]
-			j++
-		}
-		if sum == target{
-			cur_step := l - (j - i)
-			if cur_step < steps1{
-				steps1 = cur_step
-			}
-		}
-		sum -= nums[i]
-		i++
-	}
-	var steps2 int = math.MaxInt32
-	i = l - 1
-	j = l - 1
-	sum = 0
-	for i >=0 && j >= 0{
-		for i >= 0 && sum < target{
-			sum += nums[i]
-			i--
-		}
-		if sum == target{
-			cur_step := l - (j - i)
-			if cur_step < steps2{
-				steps2 = cur_step
-			}
-			break
-		}
-		sum -= nums[j]
-		j--
-	}
-	if steps1 == math.MaxInt32 && steps2 == math.MaxInt32{
-		return -1
-	}
-	if steps1 == math.MaxInt32{
-		return steps2
-	}
-	if steps2 == math.MaxInt32{
-		return steps1
-	}
-	if steps1 < steps2{
-		return steps1
-	}else{
-		return steps2
-	}
+	return l - max_len
 }
+
+//func MinOperations1658(nums []int, x int) int{
+//	var l int = len(nums)
+//	var total int = 0
+//	for i := 0;i < l;i++{
+//		total += nums[i]
+//	}
+//	var target int = total - x
+//	if target < 0{
+//		return -1
+//	}
+//	i := 0
+//	j := 0
+//	var sum int = 0
+//	var steps1 int = math.MaxInt32
+//	for i < l && j < l{
+//		for j < l && sum < target{
+//			sum += nums[j]
+//			j++
+//		}
+//		if sum == target{
+//			cur_step := l - (j - i)
+//			if cur_step < steps1{
+//				steps1 = cur_step
+//			}
+//		}
+//		sum -= nums[i]
+//		i++
+//	}
+//	var steps2 int = math.MaxInt32
+//	i = l - 1
+//	j = l - 1
+//	sum = 0
+//	for i >=0 && j >= 0{
+//		for i >= 0 && sum < target{
+//			sum += nums[i]
+//			i--
+//		}
+//		if sum == target{
+//			cur_step := l - (j - i)
+//			if cur_step < steps2{
+//				steps2 = cur_step
+//			}
+//			break
+//		}
+//		sum -= nums[j]
+//		j--
+//	}
+//	if steps1 == math.MaxInt32 && steps2 == math.MaxInt32{
+//		return -1
+//	}
+//	if steps1 == math.MaxInt32{
+//		return steps2
+//	}
+//	if steps2 == math.MaxInt32{
+//		return steps1
+//	}
+//	if steps1 < steps2{
+//		return steps1
+//	}else{
+//		return steps2
+//	}
+//}
 
 //func dp_minOperations(nums []int,head int,tail int,choose_head bool,x int)int{
 //	if x == 0{
--- src/golang/array/268. Missing Number.go
+++ src/golang/array/268. Missing Number.go
@@ -3,6 +3,8 @@ package array
 //Input: nums = [3,0,1]
 //Output: 2
 //0,1,2,3 ... n
+
+//Sum
 func missingNumber(nums []int) int {
 	var l int = len(nums)
 	var cur_sum int = 0
@@ -11,4 +13,14 @@ func missingNumber(nums []int) int {
 	}
 	var expect_sum int = ((l + 1) * l)/2
 	return expect_sum - cur_sum
+}
+
+//XOR  i * nums[i]
+func missingNumber2(nums []int) int {
+	var res int = 0
+	var l int = len(nums)
+	for i := 0;i < l;i++{
+		res = res ^ i ^ nums[i]
+	}
+	return res ^ l
 }
