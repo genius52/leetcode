f3d86a31304d8fb4a421fb848e51e6aaa9acf077
genius52 <genius52@qq.com>
2022-07-06 14:56:51 +0800

    54 optimize code style

--- src/golang/array/54. Spiral Matrix.go
+++ src/golang/array/54. Spiral Matrix.go
@@ -89,3 +89,42 @@ func spiralOrder(matrix [][]int) []int{
 	}
 	return res
 }
+
+
+func spiralOrder2(matrix [][]int) []int{
+	var rows int = len(matrix)
+	var columns int = len(matrix[0])
+	var res []int = make([]int, rows * columns)
+	var idx int = 0
+	var dirs [][]int = [][]int{{0,1},{1,0},{0,-1},{-1,0}}
+	var dir_idx int = 0
+	var r_start int = 0
+	var c_start int = 0
+	var r_end int = rows - 1
+	var c_end int = columns - 1
+	var r int = 0
+	var c int = 0
+	for idx < rows * columns{
+		for r >= r_start && r <= r_end && c >= c_start && c <= c_end {
+			res[idx] = matrix[r][c]
+			r += dirs[dir_idx][0]
+			c += dirs[dir_idx][1]
+			idx++
+		}
+		r -= dirs[dir_idx][0]
+		c -= dirs[dir_idx][1]
+		if dir_idx == 0{
+			r_start++
+		}else if dir_idx == 1{
+			c_end--
+		}else if dir_idx == 2{
+			r_end--
+		}else if dir_idx == 3{
+			c_start++
+		}
+		dir_idx = (dir_idx + 1) % 4
+		r += dirs[dir_idx][0]
+		c += dirs[dir_idx][1]
+	}
+	return res
+}
